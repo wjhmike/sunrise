3116eed842c25b5e4d866f60020a25bde8ef6ecf
wjhmike <junhan-wang@uiowa.edu>
2018-02-21 17:19:55 -0600

    simple GUI done

--- app/src/main/java/goldsquadron/sunrise/MainActivity.java
+++ app/src/main/java/goldsquadron/sunrise/MainActivity.java
@@ -1,13 +1,45 @@
 package goldsquadron.sunrise;
+import android.content.Intent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button alButton;
+    private Button cancelButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        alButton = (Button) findViewById(R.id.alButton);
+        alButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAlarmPage();
+            }
+        });
+
+        /*cancelButton = findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancelActivity();
+            }
+        });*/
     }
+
+    public void openAlarmPage(){
+        Intent intent = new Intent(this, alarmActivity.class);
+        startActivity(intent);
+    }
+
+    /*public void cancelActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }*/
+
 }
---
+++ app/src/main/java/goldsquadron/sunrise/alarmActivity.java
@@ -0,0 +1,13 @@
+package goldsquadron.sunrise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class alarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
+}
